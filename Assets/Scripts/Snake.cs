e37ee704f0388be3276cc9d644e8c530162c356e
CreatNatoy <76531899+CreatNatoy@users.noreply.github.com>
2022-02-13 07:21:24 +0100

    Initial commit

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TailGenerator))]
+public class Snake : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private float _tailSpringiness;
+    [SerializeField] private SnakeHead _head; 
+
+    private List<Segment> _tail;
+    private TailGenerator _tailGenerator;
+
+    private void Awake()
+    {
+        _tailGenerator = GetComponent<TailGenerator>(); 
+        _tail = _tailGenerator.Generate(); 
+    }
+
+    private void FixedUpdate()
+    {
+        Move(_head.transform.position + _head.transform.up * _speed * Time.fixedDeltaTime);
+    }
+
+    private void Move(Vector3 nextPosition)
+    {
+        Vector3 previousPosition = _head.transform.position; 
+
+        foreach(var segment in _tail)
+        {
+            Vector3 tempPosition = segment.transform.position;
+            segment.transform.position = Vector2.Lerp(segment.transform.position, previousPosition, _tailSpringiness * Time.deltaTime);
+            previousPosition = tempPosition; 
+        }
+
+        _head.Move(nextPosition); 
+    }
+}
